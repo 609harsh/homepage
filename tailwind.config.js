62ad99854478b5350c57840f11aef0c374f029a7
609harsh <harshgupta609@gmail.com>
2023-02-24 00:49:28 +0530

    test

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+
+  content: ['./src/**/*.{jsx,jsx,tsx,ts}',
+            './src/**/*.{jsx,js,tsx,ts}'
+  ],
+
+  theme: {
+    extend: {},
+    colors: {
+      
+      purple:'#a026ff',
+      green:'#7ee687',
+      grey:'#aaa',
+      white:'#fff',
+      blue:'#0d99ff',
+      aquaBlue:'#01a99d',
+      black:'#000'
+
+    },
+    fontFamily:{
+      mons:'Montserrat,sans-serif',
+    },
+    
+  },
+  plugins: [],
+}
